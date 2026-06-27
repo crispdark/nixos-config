20544d1d8a25fe2a90519e540b4094789319393d
Edoardo Piscitelli <haxagd@gmail.com>
2026-06-27 15:46:01 +0200

    zsh.nix optimizations

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -13,18 +13,9 @@
     };
   };
 
-  home.packages = with pkgs; [
-    zsh-powerlevel10k
-  ];
-
-  home.file.".p10k.zsh".source = ./p10k.zsh;
-
   programs.zsh.initContent = ''
     source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
     [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
   '';
-
- 
-
 }
 
