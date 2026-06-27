07691d8fa25dfea45c490f8ceb9e4d32bfb9a5b6
Edoardo Piscitelli <haxagd@gmail.com>
2026-06-27 15:36:15 +0200

    cleaning zsh.nix file

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -7,27 +7,20 @@
 
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-
-
-    oh-my-zsh = {
-      enable = true;
-      theme = "";
-
-      plugins = [
-        "git"
-      ];
-    };
   };
-
-  programs.zsh.initContent = ''
-    source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-    [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
-  '';
 
   home.packages = with pkgs; [
     zsh-powerlevel10k
   ];
 
   home.file.".p10k.zsh".source = ./p10k.zsh;
+
+  programs.zsh.initContent = ''
+    source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+    [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
+  '';
+
+ 
+
 }
 
