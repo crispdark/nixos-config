a8f5c161acf9e5b4586ed3e943265d7998ba22b4
Edoardo Piscitelli <haxagd@gmail.com>
2026-06-28 09:53:31 +0200

    excluding gnome packages

--- modules/system/gnome.nix
+++ modules/system/gnome.nix
@@ -4,4 +4,5 @@
   services.xserver.enable = true;
   services.desktopManager.gnome.enable = true;
   services.displayManager.gdm.enable = true;
+  environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs epiphany gnome-help ];
 }
