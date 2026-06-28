751fc47b2920992ccdb199087a0fc40b2983f0c4
Edoardo Piscitelli <haxagd@gmail.com>
2026-06-28 09:57:22 +0200

    removing yelp from gnome

--- modules/system/gnome.nix
+++ modules/system/gnome.nix
@@ -4,5 +4,5 @@
   services.xserver.enable = true;
   services.desktopManager.gnome.enable = true;
   services.displayManager.gdm.enable = true;
-  environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs epiphany ];
+  environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs epiphany yelp ];
 }
