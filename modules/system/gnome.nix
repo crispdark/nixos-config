c8392a7d23e7d46e78ee93177a5d6b77f554d6d6
Edoardo Piscitelli <haxagd@gmail.com>
2026-06-28 10:22:47 +0200

    adding gnome-extension to gnome.nix module

--- modules/system/gnome.nix
+++ modules/system/gnome.nix
@@ -4,5 +4,32 @@
   services.xserver.enable = true;
   services.desktopManager.gnome.enable = true;
   services.displayManager.gdm.enable = true;
-  environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs epiphany yelp ];
+
+  environment.gnome.excludePackages = with pkgs; [
+      gnome-tour
+      gnome-user-docs
+      epiphany
+      yelp
+  ];
+    
+  home.packages = with pkgs; [
+      gnomeExtensions.appindicator
+      gnomeExtensions.blur-my-shell
+      gnomeExtensions.app-hider
+      gnomeExtensions.dash-to-dock
+      gnomeExtensions.in-picture
+      gnomeExtensions.night-theme-switcher
+  ];
+  
+#  dconf.settings = {
+#    "org/gnome/shell" = {
+#      enabled-extensions = [
+#        "appindicatorsupport@rgcjonas@gmail.com"
+#        "blur-my-shell@aunetx"
+#        "app-hider@lynith.dev"
+#        "dash-to-dock@micxgx.gmail.com"
+#        "nighthemeswitcher@romainvigier.fr"
+#      ];
+#    };
+#  };
 }
