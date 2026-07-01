ec6375526072e1b660d02364b893ee4e16fc3781
Edoardo Piscitelli <haxagd@gmail.com>
2026-07-01 11:47:04 +0200

    hiding something

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -3,11 +3,5 @@
 {
   programs.git = {
     enable = true;
-
-    settings = {
-      safe.directory = "/etc/nixos";
-      user.name = "Edoardo Piscitelli";
-      user.email = "haxagd@gmail.com";
-    };
   };  
 }
