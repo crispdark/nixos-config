e7a300f5c4bcfe72d9f25868ed4a1bf49c3f2bcc
Edoardo Piscitelli <haxagd@gmail.com>
2026-07-03 12:59:42 +0200

    adding full name to users.nix

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -3,6 +3,8 @@
 {
   users.users.crispdark = {
     isNormalUser = true;
+    home = "home/crispdark"
+    description = "Edoardo Piscitelli"
     extraGroups = [ "wheel" "networkmanager" ];
     shell = pkgs.zsh;
   };
